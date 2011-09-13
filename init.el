37c9af055e6bb6c91d314c05a33aeee7de4ad6c0
Luke Randall <luke.randall@gmail.com>
2011-09-13 11:14:39 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,33 @@
+;; turn off mouse interface
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; setup load path
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+
+;; bindings
+(load "bindings")
+
+;; elpa
+(add-to-list 'load-path (concat dotfiles-dir "/elpa"))
+(require 'package)
+(package-initialize)
+
+;; smex
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commmands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; Old M-x
+
+;; ido
+(setq ido-enable-flex-matching t)
+  (setq ido-everywhere t)
+  (ido-mode 1)
+
+;; haml & sass
+(require 'haml-mode)
+(require 'sass-mode)
